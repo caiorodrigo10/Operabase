6027bc19354734e58db0eb3eb00e22bf5bfe0ad5
caiorodrigo10 <cr@caiorodrigo.com.br>
2025-07-03 17:38:56 -0300

    Remove API directory and vercel.json for pure frontend deployment

--- api/index.js
+++
@@ -1,8 +0,0 @@
-// Set Vercel environment flag
-process.env.VERCEL = 'true';
-
-// Import the Express app
-require('../dist/index.js');
-
-// Export the app that was made globally available
-module.exports = global.vercelApp; 
